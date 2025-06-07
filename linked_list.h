0a14987e2d1aaf61579b3494268482dd4810a159
Denis Shimansky <denis.shimansky@outlook.com>
2025-06-07 03:08:45 +0300

    Implemented indexers and basic methods

--- linked_list.h
+++ linked_list.h
@@ -12,7 +12,7 @@ struct node
 {
     node *prev, *next;
     T data;
-    node (const T &data): prev(0), next(0), data(data);
+    node (const T &data): prev(0), next(0), data(data) {}
 };
 
 template<typename T>
@@ -31,122 +31,270 @@ public:
     T pop_front();
     T pop_back();
 
-    T front();
-    T back();
     void insert(const T &val, size_t index);
     void unique();
-    void merge();
-    void remove_if();
+    // void merge(linked_list& other); // todo
+    // void remove_if(Predicate p); // todo
 
-    template <typename NodeType>
-    class ListIterator {
-        NodeType* current;
-        friend class LinkedList<T>; 
-        ListIterator(NodeType* ptr) : m_ptr(ptr) {}
+    template <typename NODE>
+    class linked_list_iterator {
+        NODE* current;
+        friend class linked_list<T>; 
+        linked_list_iterator(NODE* node) : current(node) {}
 
      public:
         T& operator*();
         const T& operator*() const;
-        T* operator->()
-        ListIterator operator++();
-        ListIterator& operator++(int);
-        ListIterator operator--();
-        ListIterator& operator--(int);
-        bool operator==(const ListIterator& other) const;
-        bool operator!=(const ListIterator& other) const;
-    }
+        T* operator->();
+        linked_list_iterator& operator++();
+        linked_list_iterator operator++(int);
+        linked_list_iterator& operator--();
+        linked_list_iterator operator--(int);
+        bool operator==(const linked_list_iterator& other) const;
+        bool operator!=(const linked_list_iterator& other) const;
+    };
 
-    using iterator = ListIterator<Node>;
-    using const_iterator = ListIterator<const Node>;
+    using iterator = linked_list_iterator<node<T>>;
+    using const_iterator = linked_list_iterator<const node<T>>;
 
     iterator begin();
     iterator end();
     const_iterator begin() const;
     const_iterator end() const;
 
-    T& operator[](const int index) const;
+    // T& operator[](const int index) const;
 };
 
 template<typename T>
 linked_list<T>::linked_list(): head(0), tail(0), size(0) {}
 
 template<typename T>
-linked_list<T>::~linked_list() {
-    
+linked_list<T>::~linked_list()
+{
+    node<T> *current = this->head;
+    node<T> *next = 0;
+    for (size_t i = 0; i < this->size; i++) {
+        next = current->next;
+        delete current;
+        current = next;
+    }
 }
 
 template<typename T>
 void linked_list<T>::push_front(const T &val)
 {
-    if(this->head == 0) {
-        this->head = new node<T>(val);
-        this->tail = this->head;
-        this->size++;
-        return;
+    node<T> *newItem = new node<T>(val);
+
+    if (this->head == 0) {
+        this->head = newItem;
+        this->tail = newItem;
+    } else {
+        newItem->next = this->head;
+        this->head->prev = newItem;
+        this->head = newItem;
     }
 
-    node<T> *newItem = new node<T>(val);
-    newItem->next = this->head;
-    this->head->prev = newItem;
-    this->head = newItem;
     this->size++;
 }
 
 template<typename T>
 void linked_list<T>::push_back(const T &val)
 {
+    node<T> *newItem = new node<T>(val);
+
     if (this->head == 0) {
-        this->head = new node<T>(val);
-        this->tail->prev = this->head;
-        this->size++;
-        return;
+        this->head = newItem;
+        this->tail = newItem;
+    } else {
+        this->tail->next = newItem;
+        newItem->prev = this->tail;
+        this->tail = newItem;
     }
 
-    node<T> *newItem = new node<T>(val);
-    newItem->prev = this->tail;
-    this->tail = newItem;
     this->size++;
 }
 
 template<typename T>
 T linked_list<T>::pop_front() {
-    if(this->head == 0) {
-        throw std::out_of_range("This list is empty");
+    if (this->head == 0) {
+        throw std::out_of_range("Cannot pop from an empty list");
     }
-
+    
     node<T> *item = this->head;
     T data = item->data;
     this->head = this->head->next;
-    this->head->prev = 0;
-    delete item;
     this->size--;
-    return item->data;
+
+    if (this->head != nullptr) {
+        this->head->prev = 0;
+    } else {
+        this->tail = 0;
+    }
+
+    delete item;
+    return data;
 }
 
 template<typename T>
 T linked_list<T>::pop_back() {
-    if(this->head == 0) {
-        throw std::out_of_range("This list is empty");
+    if (this->head == 0) {
+        throw std::out_of_range("Cannot pop from an empty list");
     }
-
-    if (this->head == this->tail) {
-        node<T> *item = this->head;
-        T val = item->data;
-        this->head = 0;
-        this->tail = 0;
-        delete item;
-        this->size--;
-        return val;
-    }
-
+    
     node<T> *item = this->tail;
-    T val = item->data;
-    this->tail = this->tail->prev;
-    this->tail->next = 0;
-    delete item;
+    T data = item->data;
+    this->tail = tail->prev;
     this->size--;
-    return val;
+
+    if (this->tail != 0) {
+        this->tail->next = 0;
+    } else {
+        this->head = 0;
+    }
+
+    delete item;
+    return data;
 }
 
+template<typename T>
+void linked_list<T>::insert(const T &val, size_t index)
+{
+    if (index > this->size) {
+        throw std::out_of_range("Index is out of range");
+    }
+
+    if (index == 0) {
+        this->push_front(val);
+        return;
+    }
+
+    if (index == this->size) {
+        this->push_back(val);
+        return;
+    }
+
+    node<T> *current = this->head;
+    for (size_t i = 0; i < index; i++) {
+        current = current->next;
+    }
+
+    node<T> *newItem = new node<T>(val);
+    newItem->prev = current->prev;
+    newItem->next = current;
+    current->prev = newItem;
+    newItem->prev->next = newItem;
+
+    this->size++;
+}
+
+template<typename T>
+typename linked_list<T>::iterator linked_list<T>::begin()
+{
+    return iterator(this->head);
+}
+
+template<typename T>
+typename linked_list<T>::iterator linked_list<T>::end()
+{
+    return iterator(0);
+}
+
+template<typename T>
+typename linked_list<T>::const_iterator linked_list<T>::begin() const
+{
+    return const_iterator(this->head);
+}
+
+template<typename T>
+typename linked_list<T>::const_iterator linked_list<T>::end() const
+{
+    return const_iterator(0);
+}
+
+template<typename T>
+template<typename NODE>
+T& linked_list<T>::linked_list_iterator<NODE>::operator*()
+{
+    return this->current->data;
+}
+
+template<typename T>
+template<typename NODE>
+const T& linked_list<T>::linked_list_iterator<NODE>::operator*() const
+{
+    return this->current->data;
+}
+
+template<typename T>
+template<typename NODE>
+T* linked_list<T>::linked_list_iterator<NODE>::operator->()
+{
+    return &(this->current->data);
+}
+
+template<typename T>
+template<typename NODE>
+typename linked_list<T>::linked_list_iterator<NODE>&
+linked_list<T>::linked_list_iterator<NODE>::operator++()
+{
+    current = current->next;
+    return *this;
+}
+
+template<typename T>
+template<typename NODE>
+typename linked_list<T>::linked_list_iterator<NODE>
+linked_list<T>::linked_list_iterator<NODE>::operator++(int)
+{
+    linked_list_iterator temp = *this;
+    ++(*this);
+    return temp;
+}
+
+template<typename T>
+template<typename NODE>
+typename linked_list<T>::linked_list_iterator<NODE>&
+linked_list<T>::linked_list_iterator<NODE>::operator--()
+{
+    if(current->prev == 0) {
+        throw std::out_of_range("Index is out of range");
+    }
+
+    if (current == 0) {
+        throw std::logic_error("Cannot decrement end iterator"); // todo
+    }
+
+    current = current->prev;
+    return *this;
+}
+
+template<typename T>
+template<typename NODE>
+typename linked_list<T>::linked_list_iterator<NODE>
+linked_list<T>::linked_list_iterator<NODE>::operator--(int)
+{
+    if(current->prev == 0) {
+        throw std::out_of_range("Index is out of range");
+    }
+    linked_list_iterator temp = *this;
+    --(*this);
+    return temp;
+}
+
+template<typename T>
+template<typename NODE>
+bool linked_list<T>::linked_list_iterator<NODE>
+::operator==(const linked_list_iterator& other) const
+{
+    return this->current == other.current;
+}
+
+template<typename T>
+template<typename NODE>
+bool linked_list<T>::linked_list_iterator<NODE>
+::operator!=(const linked_list_iterator& other) const
+{
+    return !(*this == other);
+}
 
 #endif
--- semple1.cpp
+++ semple1.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
-#include "forward_list.h"
+#include "linked_list.h"
 
 using std::cout, std::endl;
 
-void print_list(const forward_list<int> &list);
+int main()
+{
 
-int main() {
-    forward_list<int> list;
-    for (int i = 0; i < 20; i++) {
-        list.push_back(i);
+    linked_list<int> *list = new linked_list<int>();
+    for (int i = 0; i < 10; ++i) {
+        list->push_back(i);
     }
-    print_list(list);
 
-    list.remove_last();
-    print_list(list);
-    
-    list.remove_first();
-    print_list(list);
-
-    list.remove_at(10);
-    print_list(list);
-
-    list.push_back(30);
-    print_list(list);
-
-    return 0;
-}
-
-void print_list(const forward_list<int> &list) {
-    for (int i = 0; i < list.getSize(); i++) {
-        cout << list[i] << " ";
+    for (auto& i: *list) {
+        cout << i << " ";
     }
     cout << endl;
+    
+    return 0;
 }
